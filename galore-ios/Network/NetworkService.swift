bc1e6af90cf3b22fba38618329c1ce236c574122
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-11-01 18:06:48 +0100

    feat: async/await for network service execute method

--- galore-ios/Network/NetworkService.swift
+++ galore-ios/Network/NetworkService.swift
@@ -14,31 +14,18 @@ class NetworkService {
 		self.urlSession = session
 	}
 	
-	func execute<T: NetworkRequest>(_ request: T, completion: @escaping (Result<T.Response, NetworkError>) -> Void) {
-		   do {
-			   let urlRequest = try request.makeURLRequest()
-			   let task = urlSession.dataTask(with: urlRequest) { data, response, error in
-				   
-				   if let error = error {
-					   completion(.failure(.unknown(error)))
-					   return
-				   }
-				   
-				   guard let data = data else {
-					   completion(.failure(.requestFailed))
-					   return
-				   }
-				   
-				   do {
-					   let decodedResponse = try JSONDecoder().decode(T.Response.self, from: data)
-					   completion(.success(decodedResponse))
-				   } catch {
-					   completion(.failure(.decodingFailed))
-				   }
-			   }
-			   task.resume()
-		   } catch {
-			   completion(.failure(.unknown(error)))
-		   }
-	   }
+	func execute<T: NetworkRequest>(_ request: T) async throws -> T.Response {
+		let urlRequest = try request.makeURLRequest()
+		let (data, response) = try await urlSession.data(for: urlRequest)
+		
+		guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+			throw NetworkError.requestFailed
+		}
+		
+		do {
+			return try JSONDecoder().decode(T.Response.self, from: data)
+		} catch {
+			throw NetworkError.decodingFailed
+		}
+	}
 }
