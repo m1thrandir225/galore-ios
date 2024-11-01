69dd065c219b4b61a9eb27cc47712ef1a4b5b550
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-11-01 23:13:23 +0100

    feat: folder oragnization

---
+++ galore-ios/Presentation/Views/ForgotPasswordScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordScreen.swift
+//  galore-ios
+//
+//  Created by Sebastijan Zindl on 31.10.24.
+//
+
+import SwiftUI
+
+struct ForgotPasswordScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordScreen()
+}
