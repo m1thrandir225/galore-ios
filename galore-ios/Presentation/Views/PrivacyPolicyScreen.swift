a7c00178f9d52b5dc7f5d6a9e2e8ab05cce6c22d
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-12-28 20:02:17 +0100

    feat: fix title on terms & conditions screen and privacy policy screen finished

--- galore-ios/Presentation/Views/PrivacyPolicyScreen.swift
+++ galore-ios/Presentation/Views/PrivacyPolicyScreen.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 struct PrivacyPolicyScreen: View {
 	@StateObject var router: Router<TabRoutes>
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@State var titleVisible: Bool = true
+	
+	let paragraphs: [String] = [
+		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+	]
+	
+	var body: some View {
+		VStack(alignment: .leading) {
+			HStack {
+				Text("Privacy Policy")
+					.font(.system(size: 24, weight: .semibold))
+					.foregroundStyle(Color("OnBackground"))
+					.padding(24)
+					.opacity(titleVisible ? 0 : 1)
+					.animation(.easeInOut, value: titleVisible)
+				Spacer()
+			}
+			ScrollView(.vertical, showsIndicators: false) {
+				VStack(alignment: .leading, spacing: 24) {
+					TitleVisibilityCheckerView(title: "Privacy Policy", titleVisible: $titleVisible)
+					ForEach(paragraphs, id: \.self) { item in
+						Text(item)
+							.font(.system(size: 16, weight: .regular))
+							.foregroundStyle(Color("OnBackground"))
+					}.padding(.horizontal, 24)
+				}
+				
+			}
+		}
+		.background(Color("Background"))
+		.navigationBarBackButtonHidden(true)
+	}
 }
 
 #Preview {
